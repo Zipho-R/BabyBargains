c93a70186532d72d27110265f529c2c9bbb2ed92
.\.git\Zipho-R <ziphorenene03@gmail.com>
2026-05-07 10:36:11 +0200

    Allow Vercel frontend to access backend

--- server/server.js
+++ server/server.js
@@ -13,14 +13,26 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: [
+      "http://localhost:3000",
+      "https://baby-bargains.vercel.app/"
+    ],
     methods: ["GET", "POST", "PUT", "DELETE"]
   }
 });
 
 app.set("io", io);
 
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:3000",
+      "https://baby-bargains.vercel.app/"
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true
+  })
+);
 app.use(express.json());
 
 app.use("/api/products", require("./routes/productRoutes"));
